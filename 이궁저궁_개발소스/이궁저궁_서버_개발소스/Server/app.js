5af375d719d96beea2577775f5cc953ca6aba1c0
Ian Y. Choi <ianyrchoi@gmail.com>
2018-12-18 09:17:30 +0900

    소스

---
+++ 이궁저궁_개발소스/이궁저궁_서버_개발소스/Server/app.js
@@ -0,0 +1,83 @@
+/*
+ Default module
+*/
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const scheduler = require('./module/scheduler')
+app.use((req, res, next) => {
+  res.r = (result) => {
+      res.json({
+          status: true,
+          message: "success",
+          result,
+      });
+  };
+  next();
+});
+/*
+ Custom module
+*/
+const routes = require('./routes/routes');
+const jwt = require('./config/secretKey');
+
+/*
+ app.set
+*/
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+app.set('jwt-secret', jwt.secret);
+
+/*
+ app.use
+*/
+app.use(helmet());
+app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/',routes);
+function ignoreFavicon(req, res, next) {
+    if (req.originalUrl === '/favicon.ico') {
+      res.status(204).json({nope: true});
+    } else {
+      next();
+    }
+  }
+
+app.use(ignoreFavicon);
+
+// error handler
+require('./ErrorHandler')(app);
+
+app.use(function(req, res, next)
+{
+  var err = new Error('Not Found');
+  err.status = 404;
+  err.path = req.path;
+  log.error(err);
+  next(err);
+});
+
+// Error handler
+app.use(function(err, req, res, next)
+{
+  res.locals.message = err.message;
+  console.log("res.locals.message error : " + res.locals.message);
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log("res.locals.error error : " + res.locals.error);
+
+  res.status(err.status || 500);
+  res.render('error',{errLog : res.locals.error});
+});
+
+module.exports = app;
---
+++ 이궁저궁_개발소스/이궁저궁_서버_개발소스/Server/routes/routes.js
@@ -0,0 +1,11 @@
+/*
+ Default module
+*/
+const express = require('express');
+const router = express.Router();
+
+router.use('/user', require('./user/user_routes'));
+router.use('/ticket', require('./ticket/ticket_routes'));
+router.use('/palace', require('./palace/palace_routes.js'));
+
+module.exports = router;
